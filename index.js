bb69b368e9ddf00f32247f301e830860b3f8eeca
unknown <Ryan Vu>
2024-08-12 18:19:33 -0500

    Basically finished

--- index.js
+++ index.js
@@ -1,13 +1,117 @@
 // TODO: Include packages needed for this application
+// We need 'inquirer' for prompting user input and 'fs' for file system operations.
+import inquirer from 'inquirer';
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Define the questions to ask the user using Inquirer.
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this application?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// This function writes the README content to a file.
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('README.md created successfully!');
+        }
+    });
+}
+
+// Function to generate README content based on user input
+function generateREADME(answers) {
+    return `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions, please reach out:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+  `;
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// This function initializes the app by prompting the user and generating the README.
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateREADME(answers);
+        writeToFile('README.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
